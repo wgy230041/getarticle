a982b18b73e1d956f21d4132d7f51b90877e494c
Hao Tian <haot@smu.edu>
2020-07-13 08:13:38 -0500

    Update setup.py
    default packages like "re" and "time" cannot be in the install_requires

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setuptools.setup(
             website address or keywords.",
       long_description=long_description,
       long_description_content_type="text/markdown",
-      version='0.0.2',
+      version='0.0.4',
       url='https://github.com/HTian1997/getarticle',
       author='Hao Tian',
       author_email='htian1997@gmail.com',
       license='MIT',
       packages=setuptools.find_packages(),
-      install_requires=["requests", "re", "time"],
+      install_requires=["requests"],
       zip_safe=False
 )
