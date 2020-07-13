9cb841ac62bc69cb95e58217ab9db82aa0600473
Hao Tian <haot@smu.edu>
2020-07-13 08:00:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(name='getarticle',
-	description="getarticle is a package based on SciHub and Google Scholar\
-	   that can download articles based on DOI or website address. \
-	   It can also download related articles given keywords.",
+setuptools.setup(
+      name='getarticle',
+      description="A package based on SciHub and Google \
+            Scholar that can download articles given DOI, \
+            website address or keywords.",
       long_description=long_description,
       long_description_content_type="text/markdown",
       version='0.0.2',
@@ -15,6 +16,6 @@ setuptools.setup(name='getarticle',
       author_email='htian1997@gmail.com',
       license='MIT',
       packages=setuptools.find_packages(),
-      install_requires=['requests']
+      install_requires=["requests", "re", "time"],
       zip_safe=False
 )
